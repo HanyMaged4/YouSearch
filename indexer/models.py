3b4a7b7620132d59c8db332fe113be3444fad24b
Hany Maged <hany.maged9102@gmail.com>
2026-01-15 14:11:04 +0200

    make the Urls unique

--- indexer/models.py
+++ indexer/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Videos(models.Model):
     videoID = models.CharField(max_length=64 , primary_key=True)
     title = models.CharField(max_length=255)
-    videoURL = models.URLField(max_length=500,)
+    videoURL = models.URLField(max_length=500,unique=True)
 
     def __str__(self):
         return f"{self.title} ({self.videoID})"
