a8e63e208092ba8f81f04c0fb7369cebdf04cb08
Hany Maged <hany.maged9102@gmail.com>
2026-01-15 13:01:02 +0200

    fix models

--- indexer/models.py
+++ indexer/models.py
@@ -7,25 +7,31 @@ class Videos(models.Model):
     videoURL = models.URLField(max_length=500)
 
     def __str__(self):
-        return f"{self.title} ({self.videoId})"
+        return f"{self.title} ({self.videoID})"
     
 
 class Words(models.Model):
     wordID = models.AutoField(primary_key=True)
-    word = models.CharField(max_length=255)
+    word = models.CharField(max_length=255, unique=True, db_index=True)
     cnt = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.word} ({self.WordID})"
+        return f"{self.word} ({self.wordID})"
     
-class Indexing(models.Model):
-    word = models.ForeignKey(Words , on_delete=models.CASCADE)
+
+class Sentences(models.Model):
+    id = models.AutoField(primary_key=True)
     video = models.ForeignKey(Videos , on_delete=models.CASCADE)
+    sentence = models.CharField(max_length=2064)
     start = models.FloatField()
     end = models.FloatField()
+
+class Indexing(models.Model):
+    word = models.ForeignKey(Words , on_delete=models.CASCADE)
+    sentence = models.ForeignKey(Sentences , on_delete=models.CASCADE)
     
     class Meta:
-        unique_together = ('word', 'video', 'start', 'end')
+        unique_together = ('word', 'sentence')
     
     def __str__(self):
-        return f"{self.word.word} in {self.video.videoId} [{self.start}-{self.end}]"
+        return f"{self.word.word} in {self.video.videoID} [{self.start}-{self.end}]"
